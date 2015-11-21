874b79661cdd4d9daac6813a3956e1635fc38214
Don Griffin <donggriffin@Dons-MacBook-Pro.local>
2015-11-21 15:17:57 -0500

    Adding marker placements for stops.

--- client/templates/mbta-penguin/penguin.js
+++ client/templates/mbta-penguin/penguin.js
@@ -1,6 +1,6 @@
 Template.penguin.onRendered(function() {
     GoogleMaps.load({
-    	libraries: 'geometry,places'
+        libraries: 'geometry,places'
     });
 });
 
@@ -8,64 +8,21 @@ Template.penguin.onCreated(function() {
 
     GoogleMaps.ready('exampleMap', function(map) {
         var marker = new google.maps.Marker({
-            position: map.options.center,
+            position : {lat: parseFloat(Stops.findOne().stop_lat), lng: parseFloat(Stops.findOne().stop_lon)},
+            draggable: true,
             map: map.instance
         });
 
-        var input = document.getElementById('pac-input');
-        var searchBox = new google.maps.places.SearchBox(input);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        var stops = Stops.find().fetch();
 
-        map.addListener('bounds_changed', function() {
-            searchBox.setBounds(map.getBounds());
-        });
-
-        var markers = [];
-        // Listen for the event fired when the user selects a prediction and retrieve
-        // more details for that place.
-        searchBox.addListener('places_changed', function() {
-            var places = searchBox.getPlaces();
-
-            if (places.length == 0) {
-                return;
-            }
-
-            // Clear out the old markers.
-            markers.forEach(function(marker) {
-                marker.setMap(null);
+        _.each(stops, function(stop){
+        	debugger;
+            new google.maps.Marker({
+                //position: new google.maps.LatLng(parseFloat(stop.stop_lat), parseFloat(stop.stop_lon)),
+                position: {lat: parseFloat(stop.stop_lat), lng: parseFloat(stop.stop_lon)},
+                map: map.instance
             });
-            markers = [];
-
-
-            // For each place, get the icon, name and location.
-            var bounds = new google.maps.LatLngBounds();
-            places.forEach(function(place) {
-                var icon = {
-                    url: place.icon,
-                    size: new google.maps.Size(71, 71),
-                    origin: new google.maps.Point(0, 0),
-                    anchor: new google.maps.Point(17, 34),
-                    scaledSize: new google.maps.Size(25, 25)
-                };
-
-                // Create a marker for each place.
-                markers.push(new google.maps.Marker({
-                    map: map,
-                    icon: icon,
-                    title: place.name,
-                    position: place.geometry.location
-                }));
-
-                if (place.geometry.viewport) {
-                    // Only geocodes have viewport.
-                    bounds.union(place.geometry.viewport);
-                } else {
-                    bounds.extend(place.geometry.location);
-                }
-            });
-            map.fitBounds(bounds);
-        });
-
+        })
     });
 });
 
@@ -80,5 +37,4 @@ Template.penguin.helpers({
             };
         }
     }
-
 });
