b2bb68847c3a610d1cd05741bc4d507db98dc40e
Christina Kayastha <ckayastha@vistaprint.com>
2015-11-21 15:32:27 -0500

    adding routes

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ Meteor.startup(function() {
     var api_key = "wX9NwuHnZU2ToO7GmGR9uw"
     var format = "json"
 
-    var routes = ["1"];
+    var routes = ["1", "15", "22", "23", "28", "32", "39", "57", "66", "71", "73", "111"];
     // TODO: add routes:
     // Silver Line, 116/117
     
