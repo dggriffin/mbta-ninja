ba1b49355287f31d9bca5e1dae27e394b8b4ca49
Christina Kayastha <ckayastha@vistaprint.com>
2015-11-21 14:39:48 -0500

    Adding spots from MTBA API

--- client/templates/mbta-penguin/penguin.js
+++ client/templates/mbta-penguin/penguin.js
@@ -8,9 +8,10 @@ Template.penguin.helpers({
         if (GoogleMaps.loaded()) {
             // Map initialization options
             return {
-                center: new google.maps.LatLng(-37.8136, 144.9631),
+                center: new google.maps.LatLng(42.3597105, -71.0529804),
                 zoom: 15
             };
         }
     }
+
 });
--- server/server.js
+++ server/server.js
@@ -13,6 +13,39 @@ Meteor.startup(function() {
       return {statusCode:200, body:{data: reports}};
     }
   });
+
+
+
+  // If there database is not yet populated then add info from the MBTA API
+  if(_.isUndefined(Stops.findOne())){
+    var url = "http://realtime.mbta.com/developer/api/v2/stopsbyroute"
+    var api_key = "wX9NwuHnZU2ToO7GmGR9uw"
+    var format = "json"
+
+    var routes = ["1"];
+    // TODO: add routes:
+    // Silver Line, 1, 15, 22, 23, 28, 32, 39, 57, 66, 71, 73, 111, 116/117
+    
+    _.each(routes, function(route){
+      var data = Meteor.http.get(url + "?api_key=" + api_key + "&format=" + format + "&route=" + route).data;
+      console.log(data)
+
+      var directions = data.direction;
+      _.each(directions, function(direction){
+        var stops = direction.stop;
+          _.each(stops, function(stop){
+            console.log(stop)
+            Stops.insert({ 
+              stop_id : stop.stop_id,
+              stop_name: stop.stop_name,
+              stop_lat: stop.stop_lat,
+              stop_lon: stop.stop_lon
+            });
+          })
+      });  
+    });   
+  }
+
 });
 
 // Task to expire old reports. It checks every minute to see if there are
