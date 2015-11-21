912b1e565c98147497957b664920ed321419953a
Don Griffin <donggriffin@Dons-MacBook-Pro.local>
2015-11-21 15:58:35 -0500

    Adding jump to location for google map places search.

--- client/templates/mbta-penguin/penguin.js
+++ client/templates/mbta-penguin/penguin.js
@@ -9,56 +9,36 @@ Template.penguin.onCreated(function() {
     GoogleMaps.ready('exampleMap', function(map) {
         var stops = Stops.find().fetch();
 
-        _.each(stops, function(stop){
+        _.each(stops, function(stop) {
             new google.maps.Marker({
-                position: {lat: parseFloat(stop.stop_lat), lng: parseFloat(stop.stop_lon)},
+                position: {
+                    lat: parseFloat(stop.stop_lat),
+                    lng: parseFloat(stop.stop_lon)
+                },
                 map: map.instance
             });
-        })
+        });
 
         var input = document.getElementById('pac-input');
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-        map.addListener('bounds_changed', function() {
-            searchBox.setBounds(map.getBounds());
-        });
-        
+        var searchBox = new google.maps.places.SearchBox(input);
 
-        var markers = [];
-        // Listen for the event fired when the user selects a prediction and retrieve
-        // more details for that place.
+        map.instance.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+
+        map.instance.addListener('bounds_changed', function() {
+            searchBox.setBounds(map.instance.getBounds());
+        });
+
         searchBox.addListener('places_changed', function() {
             var places = searchBox.getPlaces();
 
             if (places.length == 0) {
                 return;
             }
-
-            // Clear out the old markers.
-            markers.forEach(function(marker) {
-                marker.setMap(null);
-            });
-            markers = [];
-
-
+            
             // For each place, get the icon, name and location.
             var bounds = new google.maps.LatLngBounds();
             places.forEach(function(place) {
-                var icon = {
-                    url: place.icon,
-                    size: new google.maps.Size(71, 71),
-                    origin: new google.maps.Point(0, 0),
-                    anchor: new google.maps.Point(17, 34),
-                    scaledSize: new google.maps.Size(25, 25)
-                };
-
-                // Create a marker for each place.
-                markers.push(new google.maps.Marker({
-                    map: map,
-                    icon: icon,
-                    title: place.name,
-                    position: place.geometry.location
-                }));
 
                 if (place.geometry.viewport) {
                     // Only geocoes have viewport.
@@ -69,6 +49,8 @@ Template.penguin.onCreated(function() {
 
                 console.log(place.name)
             });
+                map.instance.fitBounds(bounds);
+
         })
     });
 });
