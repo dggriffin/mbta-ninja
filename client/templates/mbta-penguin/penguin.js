2cab6baa86c56f4e3a27a4cb47c2c3cefa3069ae
Don Griffin <donggriffin@Dons-MacBook-Pro.local>
2015-11-21 13:55:07 -0500

    Initial google map.

---
+++ client/templates/mbta-penguin/penguin.js
@@ -0,0 +1,16 @@
+Template.penguin.onRendered(function() {
+    GoogleMaps.load();
+});
+
+Template.penguin.helpers({
+    exampleMapOptions: function() {
+        // Make sure the maps API has loaded
+        if (GoogleMaps.loaded()) {
+            // Map initialization options
+            return {
+                center: new google.maps.LatLng(-37.8136, 144.9631),
+                zoom: 15
+            };
+        }
+    }
+});
